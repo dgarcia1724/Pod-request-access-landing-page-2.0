127025a07c9fd96db575f7660bfbeebe66b68594
dgarcia1724 <dgarcia1724@berkeley.edu>
2023-05-08 22:42:54 -0700

    victory

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const mobileError = document.querySelector(".mobileError");
 btnMobile.addEventListener("click", function () {
   if (inputMobile.value === "") {
     mobileError.textContent = "Oops! Please add your email";
-  } else if (inputMobile.value === "23") {
-    mobileError.textContent = "Michael Jordan";
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(inputMobile.value)) {
+    mobileError.textContent = "Oops! Please check your email";
   } else {
     mobileError.textContent = "";
   }
